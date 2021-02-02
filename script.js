478da000bd1b087da714bc71c637d93d13e3fce9
Ansuman Dhar <48406323+Ansuman-Dhar@users.noreply.github.com>
2021-02-02 09:10:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 var lat = 21.4703944;
 var lon = 83.9750000;
-const apiKey = 'bqb1rqr8ggh0hnk4a3v5dc3all';
+const apiKey = '7d54a48c-2ffe-4dc8-830d-b1c6a3607e33';
 
 const html_temp = document.getElementById("temp");
 const html_aqi = document.getElementById("aqi");
-const html_update = document.getElementById("update");
 
-const data_url = "http://api.airpollutionapi.com/1.0/aqi?lat=" +lat+ "&lon=" +lon+ "&APPID=" +apiKey;
+const data_url = "https://api.airvisual.com/v2/nearest_city?lat=" +lat+ "&lon=" +lon+ "&key=" +apiKey;
 
 //Map
 var map = L.map('map').setView([lat, lon], 15);
@@ -20,14 +19,11 @@ async function main(){
 	var response = await fetch(data_url);
 	var statistics = await response.json();
 	
-	const temp = statistics.data.temp;
+	const temp = statistics.data.current.weather.tp;
 	html_temp.textContent = temp + '\u2103';
 	
-	const aqi = statistics.data.value;
+	const aqi = statistics.data.current.pollution.aqius;
 	html_aqi.textContent = aqi;
-	
-	const update = statistics.data.updated;
-	html_update.textContent = update.slice(0, update.length - 10);
 }
 
 main();
